4ebe8a3a3ae09c97f47beb6c4baa8958bc342b77
azavrsnik <andrej@zavrsnik.si>
2024-02-15 08:29:38 +0100

    Removing premium mode / setting Fliqa docker

--- lib/lago_utils/lago_utils/license.rb
+++ lib/lago_utils/lago_utils/license.rb
@@ -8,12 +8,16 @@ module LagoUtils
     end
 
     def verify
+      @premium = true
+      return true
+=begin
       return if ENV['LAGO_LICENSE'].blank?
 
       http_client = LagoHttpClient::Client.new("#{url}/verify/#{ENV['LAGO_LICENSE']}")
       response = http_client.get
 
       @premium = response['valid']
+=end
     end
 
     def premium?
